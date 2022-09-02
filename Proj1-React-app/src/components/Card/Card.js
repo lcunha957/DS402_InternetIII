cc6cfd46578d3ce1a1723af10af885689ac5f363
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-09-02 19:29:54 -0300

    exercicio 3.2 da apostila 02 react
    .

--- Proj1-React-app/src/components/Card/Card.js
+++ Proj1-React-app/src/components/Card/Card.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Card.css";
-
 export default function Card(props) {
-   return(
-       <div className="card">
-           <div className="titulo">{props.titulo}</div>
-           <div className="conteudo">{props.children}</div>
-       </div>
-   ) 
+const estilo = {
+    backgroundColor: "#5F9EA0",
+    borderColor: "#5F9EA0"
 }
+return(
+  
+<div className="card" style={estilo}>
+    <div className="titulo"> { props.titulo} </div>
+    <div className="conteudo"> { props.children }</div>
+</div>
+); }
