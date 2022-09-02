fe7991b2174bfabf2f5b187b20642cd0bbad1c48
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-09-02 20:46:29 -0300

    tentativa4 com react-bootstrap
    .

--- Proj1-React-app/src/components/Card/Card.js
+++ Proj1-React-app/src/components/Card/Card.js
@@ -6,27 +6,28 @@ import NumeroAleatorio from "../NumeroAleatorio";
 import PalavraAleatoria from "../PalavraAleatoria";
 import CoresAleatorias from "../CoresAleatorias";
  //Exercício 3.2. conforme apostila 2 de react
-export default function Cards(props) {
-const estilo = {
-    backgroundColor: "#5F9EA0",
-    borderColor: "#5F9EA0",
-    width: "25 rem"
-}
+const Cards() => {
 
 const cardInfo = [
     {title:"Exemplo01" , body: Exemplo01},
     {title: "Numero Aleatório", body: NumeroAleatorio },
     {title: "Palavra Aleatória", body: PalavraAleatoria},
     {title: "Cores Aleatorias", body: CoresAleatorias},
-]
+];
+const estilo = {
+    backgroundColor: "#5F9EA0",
+    borderColor: "#5F9EA0",
+    width: "25 rem"
+},
+
 
 const renderCard = (card, index) => {
     return(  
     <Card style = {{estilo}} key ={index}>
-        <Card.body>
-        <Card.title>{card.title}</Card.title>
-        <Card.text>{card.text}</Card.text> 
-        </Card.body>
+        <Card.Body>
+        <Card.Title>{card.title}</Card.Title>
+        <Card.Text>{card.text}</Card.Text> 
+        </Card.Body>
          </Card>
          )
   
@@ -46,4 +47,6 @@ return <div className="App">{cardInfo.map(renderCard)}</div>
  */
 
 };
+
+export default Cards; 
 
