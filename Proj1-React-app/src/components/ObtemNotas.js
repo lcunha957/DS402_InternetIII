8d5385889ce2b051a9ee701bb40dbc4786975574
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-09-02 22:31:17 -0300

    css do obtem notas
    .

--- Proj1-React-app/src/components/ObtemNotas.js
+++ Proj1-React-app/src/components/ObtemNotas.js
@@ -3,12 +3,8 @@ import React from 'react';
 export default function ObtemNotas(props){
 // props.quandoClicar vai informar alguma coisa para o componente pai return(
 return(
-<div style={{ backgroundColor: '#FAF0E6', padding: '5px' }}> 
+<div  id = "divDoObtemNotas"> 
 <h2>Obtem Notas (filho)</h2>
-<button id="btnNotas" style="margin: 20px; border-radius: 10px; background-color:#BD5784; color:#FF9800 " onClick={
-    function (e) {
-        props.quandoClicar('Maria',5);
-    }
-}>Obter nota</button>
+<button id="btnNotas" onClick={ function (e) { props.quandoClicar('Maria',5);}}>Obter nota</button>
 </div>
 )};
