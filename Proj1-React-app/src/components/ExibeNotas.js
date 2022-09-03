78fe19040c47e3d90adedf72da7f0c22d34e6cbd
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-09-02 22:05:58 -0300

    pagina 7, apostila 3 react
    .

---
+++ Proj1-React-app/src/components/ExibeNotas.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ObtemNotas from './ObtemNotas';
+
+
+export default function ExibeNotas(props){
+function Informacoes(nome,nota) { console.log(nome, nota);
+}
+return(
+<div style={{ backgroundColor: '#E0FFFF', padding: '5px' }}>
+<h2>Exibe notas (pai)</h2>
+<ObtemNotas quandoClicar={ Informacoes } ></ObtemNotas>
+</div>
+); }
---
+++ Proj1-React-app/src/components/ObtemNotas.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function ObtemNotas(props){
+// props.quandoClicar vai informar alguma coisa para o componente pai return(
+return(
+<div style={{ backgroundColor: '#FAF0E6', padding: '5px' }}> 
+<h2>Obtem Notas (filho)</h2>
+<button  style="margin: 20px; border-radius: 10px; background-color:#BD5784; color:#FF9800 " onClick={
+    function (e) {
+        props.quandoClicar('Maria',5);
+    }
+}>Obter nota</button>
+</div>
+)};
