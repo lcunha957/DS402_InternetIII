6c3a4471c4ca941aacd9962eff53150af3d588ca
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-09-02 21:55:54 -0300

    mudando a cor do retorno de verificaIdade
    .

--- Proj1-React-app/src/components/VerificaIdade.js
+++ Proj1-React-app/src/components/VerificaIdade.js
@@ -3,7 +3,10 @@ import React from 'react';
 export default function VerificaIdade(props){
 const situacao = props.idade >=18 ? "Maior de idade":"Menor de idade"; return(
         <div>
+            <div style={{ color: "#A020F0"}} >
             <h2>{ props.nome} é { situacao } </h2>
+            </div>
+   
 </div>
 );
 }
