19f1fe4a30047cbc2381b799b9d46d3f20f03bcb
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-09-09 20:02:53 -0300

    parte 2 de montar templates (arquivos)
    página 7 da apostila 3 de react

--- Proj2-Escola/escola/src/App.js
+++ Proj2-Escola/escola/src/App.js
@@ -1,11 +1,12 @@
-
 import './App.css';
-
-
+import Logo from './components/template/Logo'; import Menu from './components/template/Menu'; import Main from './components/template/Main'; import Footer from './components/template/Footer';
 function App() {
   return (
-    <div >
-      <h1> Projeto Escola</h1>
+      <div className="App">
+        <Logo />
+        <Menu />
+        <Main />
+        <Footer />
 </div>
 ); }
 export default App;
---
+++ Proj2-Escola/escola/src/components/template/Header.js
@@ -0,0 +1,7 @@
+import './Header.css';
+
+export default function Header(props) { return(
+<header className="header">
+        Cabeçalho
+</header>
+) }
