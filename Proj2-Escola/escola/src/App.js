5d8a452d24eb7a2eff1b707311a2fb4bcd33db5a
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-09-09 20:07:48 -0300

    página 7 da apostila 3 de react
    .

--- Proj2-Escola/escola/src/App.js
+++ Proj2-Escola/escola/src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
-import Logo from './components/template/Logo'; import Menu from './components/template/Menu'; import Main from './components/template/Main'; import Footer from './components/template/Footer';
+import Logo from './components/template/Logo'; 
+import Menu from './components/template/Menu'; 
+import Main from './components/template/Main'; 
+import Footer from './components/template/Footer';
+
+
 function App() {
   return (
       <div className="App">
