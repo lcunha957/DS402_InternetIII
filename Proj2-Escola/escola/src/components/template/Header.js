56593ffa8116b861a790ec58f5e3afd0d9fd49d2
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-09-16 19:17:09 -0300

    pag5, apost4
    .

--- Proj2-Escola/escola/src/components/template/Header.js
+++ Proj2-Escola/escola/src/components/template/Header.js
@@ -1,8 +1,8 @@
 import './header.css';
 import React from 'react';
 
-export default function Header(props) { return (
-        <header className="header">
-            <h2>{props.title}</h2>
-</header>
-) }
+export default function Header(props) { return(
+    <header className="header">
+            Cabeçalho
+    </header>
+    ) }
--- Proj2-Escola/escola/src/components/template/Logo.js
+++ Proj2-Escola/escola/src/components/template/Logo.js
@@ -3,11 +3,7 @@ import React from 'react';
 import logo from '../../assets/imagens/logo_escola.png';
 
 export default function Logo(props) { return (
-        <aside className="logo">
-<a href="/" className="logo">
-    <img src={ logo } alt="Logo" />
-</a>
-
+    <aside className="logo">
+        Logo
 </aside>
-)
-}
+) }
