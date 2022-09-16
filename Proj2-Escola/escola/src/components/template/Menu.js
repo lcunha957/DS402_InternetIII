86748ba94e5029867e8dee8d34c041969af9f421
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-09-16 19:57:26 -0300

    teste de colocar o logo dentro da nav.brand
    .

--- Proj2-Escola/escola/src/components/template/Menu.js
+++ Proj2-Escola/escola/src/components/template/Menu.js
@@ -1,11 +1,14 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Logo from './Logo';
 
 function Menu() {
   return (
     <>
       <Navbar bg="dark" variant="dark">
+      <Navbar.Brand>  <img src={Logo} />
+</Navbar.Brand>
         <Container>
           <Nav className="me-auto">
             <Nav.Link href="#alunos">Alunos</Nav.Link>
