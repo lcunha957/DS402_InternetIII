488d939dd13726d2b5e2d2b71531538e7288ce39
Lunara <88806865+lcunha957@users.noreply.github.com>
2022-09-16 17:04:05 -0300

    teste css 3
    .

--- Proj2-Escola/escola/src/components/template/Menu.js
+++ Proj2-Escola/escola/src/components/template/Menu.js
@@ -6,13 +6,11 @@ import Navbar from 'react-bootstrap/Navbar';
 export default function Menu(props) { return (
      <Navbar bg="light" expand="lg">
       <Container>
-      <Navbar.Collapse id="basic-navbar-nav">
-          <nav class="menu">
+          <nav class="me-auto">
             <Nav.Link href="#alunos">Alunos</Nav.Link>
             <Nav.Link href="#cursos">Cursos</Nav.Link>
             <Nav.Link href="#carometro">Carômetro</Nav.Link>
           </nav>
-          </Navbar.Collapse>
       </Container>
     </Navbar>
 
