0e60ec34d9aca0763007b4f5b86fa5b5f1bc676c
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-09-16 19:19:49 -0300

    app.js pag6, apost4
    ;

--- Proj2-Escola/escola/src/components/template/Main.js
+++ Proj2-Escola/escola/src/components/template/Main.js
@@ -3,10 +3,12 @@ import React from 'react';
 import Header from './Header';
 
 export default function Main(props) { return (
-    ) }
+    
     <React.Fragment>
         <Header />
         <main className="content">
             Conteúdo
         </main>
     </React.Fragment>
+
+)}
