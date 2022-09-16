bb013f7ad313da7227c8d7903d1c50f46c058e7f
Lunara <88806865+lcunha957@users.noreply.github.com>
2022-09-16 16:39:09 -0300

    mudando o css do menu
    ;

--- Proj2-Escola/escola/src/components/template/Menu.js
+++ Proj2-Escola/escola/src/components/template/Menu.js
@@ -1,12 +1,14 @@
 export default function Menu(props) { return (
-  <nav className='menu'>
-      <a href="#/">
-Alunos
-      </a>
-      <a href="#/">
-Cursos
-      </a>
-      <a href="#/">
-Carômetro
-</a> </nav>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">          <li class="nav-item active">
+            <a class="nav-link" href="#"> alunos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Cursos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Carômetro</a>
+          </li>
+          </div>
+  </nav>
 ) }
