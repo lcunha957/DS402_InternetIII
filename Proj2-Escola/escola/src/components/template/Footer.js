58e6c78eeaadf509bf937a8f9e436fbb31d3ac61
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-09-16 19:35:16 -0300

    footer e menu css
    pag14, apost4 react

--- Proj2-Escola/escola/src/components/template/Footer.js
+++ Proj2-Escola/escola/src/components/template/Footer.js
@@ -3,6 +3,6 @@ import React from 'react';
 
 export default function Footer(props){ return(
     <footer className="footer">
-Rodapé
+<span> .: Using ReactJS and ASP.Net Core :. </span>
 </footer>
 ) }
