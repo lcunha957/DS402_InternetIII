4e6c301b7be18e584be847d38c655dff0b2a4928
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-09-16 20:14:30 -0300

    testando sem a classe logo
    .

--- Proj2-Escola/escola/src/App.js
+++ Proj2-Escola/escola/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Logo from './components/template/Logo'; 
+
 import Menu from './components/template/Menu'; 
 import Main from './components/template/Main'; 
 import Footer from './components/template/Footer';
@@ -8,12 +8,11 @@ import Footer from './components/template/Footer';
 function App() {
   return (
       <div className="App">
-        <Logo />
-        <Menu />
+          <Menu />
         <Main />
         <Footer />
 </div>
 );
  }
- 
+
 export default App;
--- Proj2-Escola/escola/src/components/template/Menu.js
+++ Proj2-Escola/escola/src/components/template/Menu.js
@@ -1,16 +1,17 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Logo from './Logo';
+import logotipo from './logo_escola.png';
 
 function Menu() {
   return (
     <>
       <Navbar bg="dark" variant="dark">
-      <Navbar.Brand>  <img src={Logo} />
-</Navbar.Brand>
-        <Container>
+            <Container>
           <Nav className="me-auto">
+          
+            <img alt="" src="public/logo_escola.png" 
+            width="100" height="50" className="d-inline-block align-top"></img>
             <Nav.Link href="#alunos">Alunos</Nav.Link>
             <Nav.Link href="#cursos">Cursos</Nav.Link>
             <Nav.Link href="#carometro">Carometro</Nav.Link>
