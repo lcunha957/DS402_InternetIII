d14d404fded9e238af2fa6fa08dcced84b3ed606
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-09-16 19:13:37 -0300

    pag.4, apost 4
    .

--- Proj2-Escola/escola/src/components/template/Menu.js
+++ Proj2-Escola/escola/src/components/template/Menu.js
@@ -4,14 +4,10 @@ import Navbar from 'react-bootstrap/Navbar';
 
 
 export default function Menu(props) { return (
-     <Navbar bg="light" expand="lg">
-      <Container>
-          <nav class="menu">
-            <Nav.Link href="#alunos">Alunos</Nav.Link>
-            <Nav.Link href="#cursos">Cursos</Nav.Link>
-            <Nav.Link href="#carometro">Carômetro</Nav.Link>
-          </nav>
-      </Container>
-    </Navbar>
+  <nav className='menu'>
+      Menu
+</nav>
+)
+}
 
-) }
+
