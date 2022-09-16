93e63371ebfe54509a3f93b98083346b934c1fa2
Lunara <88806865+lcunha957@users.noreply.github.com>
2022-09-16 17:00:50 -0300

    teste de css 2
    ainda falta desgrudar os itens

--- Proj2-Escola/escola/src/components/template/Menu.js
+++ Proj2-Escola/escola/src/components/template/Menu.js
@@ -6,15 +6,13 @@ import Navbar from 'react-bootstrap/Navbar';
 export default function Menu(props) { return (
      <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#alunos">React com Dot.Net</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+      <Navbar.Collapse id="basic-navbar-nav">
+          <nav class="menu">
             <Nav.Link href="#alunos">Alunos</Nav.Link>
             <Nav.Link href="#cursos">Cursos</Nav.Link>
             <Nav.Link href="#carometro">Carômetro</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+          </nav>
+          </Navbar.Collapse>
       </Container>
     </Navbar>
 
