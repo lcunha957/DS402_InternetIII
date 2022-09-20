ff095ac99839192c7b6e40ad36f95ad5fa15bc42
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-09-20 19:16:19 -0300

    logo (css e js)
    pag. 11 da apostila 4 react

--- escola/src/components/template/Logo.js
+++ escola/src/components/template/Logo.js
@@ -1,12 +1,12 @@
-import './Logo.css'; 
+import './Logo.css';
+import React from 'react';
+import logo from '../../assets/imagens/logo_escola.png'; 
 
-export default function Logo(props) {
-    
-    
-    return (
-        
-        <aside className="logo"> Logo </aside>
-        
-        )
-    
-    }
+export default function Logo(props) { return (
+    <aside className="logo">
+<a href="/" className="logo">
+<img src={ logo } alt="Logo" />
+</a>
+</aside>
+)
+}
