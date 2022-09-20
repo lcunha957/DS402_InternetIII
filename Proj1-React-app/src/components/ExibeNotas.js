f9c2f4ba35fa27f936e0ff47d08c897f393b52a1
Lunara <88806865+lcunha957@users.noreply.github.com>
2022-09-20 15:10:56 -0300

    pag 9, apostila 3 react
    .

--- Proj1-React-app/src/components/ExibeNotas.js
+++ Proj1-React-app/src/components/ExibeNotas.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState} from 'react';
 import ObtemNotas from './ObtemNotas';
 
+
 export default function ExibeNotas(props){
-function Informacoes(nome,nota) { console.log(nome, nota);
-}
-return(
-<div style={{ backgroundColor: '#E0FFFF', padding: '5px' }}>
-<h2>Exibe notas (pai)</h2>
-<ObtemNotas quandoClicar={ Informacoes } ></ObtemNotas>
-</div>
-); }
+    let situacao = 'sem definição'; 
+    const [nom, setNome] = useState("sem valor");
+    const [not, setNota] = useState("sem valor"); 
+
+if (!isNaN(not)) {
+situacao = not>=5 ? "Aprovado" : "Reprovado"; 
+} 
+    
+function Informacoes(nome,nota) { 
+    setNome(nome); setNota(nota); } 
+    return(
+    <div style={{ backgroundColor: '#E0FFFF', padding: '5px' }}> 
+    <h2>Exibe notas (pai)</h2> 
+    <ObtemNotas quandoClicar={ Informacoes } ></ObtemNotas>
+    <p> Nome: { nom } - Nota: { not } </p>
+    <p>Situação: {situacao }</p>
+    </div> );
+    }
