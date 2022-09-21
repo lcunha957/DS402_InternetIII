7ce639848ca2c22ba80fefc0c8c49b41fed7ae1d
Lunara <88806865+lcunha957@users.noreply.github.com>
2022-09-21 16:49:18 -0300

    pag19 apost4 react
    .

--- escola/src/App.js
+++ escola/src/App.js
@@ -1,26 +1,20 @@
-import './App.css';
+import "./App.css";
 
-import Logo from './components/template/Logo'; 
-import Menu from './components/template/Menu';
-import Main from './components/template/Main';
-import Footer from './components/template/Footer';
-function App() { 
-  
+import Logo from "./components/template/Logo";
+import Menu from "./components/template/Menu";
+import Main from "./components/template/Main";
+import Footer from "./components/template/Footer";
+import CrudAluno from "./components/CrudAluno/CrudAluno";
+
+function App() {
   return (
-     <div className="App"> 
-     <Logo />
-     <Menu />
-     
-     <Main title="Bem Vindo!"> 
-     <div>Cadastro de alunos, cursos e carômetro</div>
-    </Main>
-
-
-    <Footer /> 
+    <div className="App">
+      <Logo />
+      <Menu />
+      <CrudAluno />
+      <Footer />
     </div>
-      );
-    
-    } 
-    
-    
-    export default App;
+  );
+}
+
+export default App;
