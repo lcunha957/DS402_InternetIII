baa5246633b4fea2282d8fd01fce8e7d55947c5f
Lunara <88806865+lcunha957@users.noreply.github.com>
2022-09-22 02:23:54 -0300

    acrescentando crudcurso
    com base no crud aluno apost4 react

---
+++ escola/src/components/CrudCurso/CrudCurso.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+
+import "./CrudCurso.css";
+
+import Main from "../template/Main";
+
+const title = "Cadastro de Cursos";
+
+const Cursos = [
+  { id: 1, codCurso:16, nomeCurso: "Alimentos", período: "matutino" },
+  { id: 2, codCurso:19, nomeCurso: "Informática", período: "matutino" },
+  { id: 3, codCurso:39, nomeCurso: "Desenvolvimento de Sistemas", período: "vespertino" },
+  { id: 4, codCurso:59, nomeCurso: "Desenvolvimento de Sistemas", período: "noturno" },
+  
+];
+
+export default class CrudCurso extends Component {
+  renderTable() {
+    return (
+      <div className="listagemCursos">
+        <table className="listaCurso" id="tblListaCursos">
+          <thead>
+            <tr className="cabecTabela">
+              <th className="tabTitulocodCurso">Código </th>
+              <th className="tabTituloNomeCurso">Nome</th>
+              <th className="tabTituloPeríodo">Período</th>
+            </tr>
+          </thead>
+
+          <tbody>
+          {Cursos.map( 
+                      (curso) =>
+                       <tr key={curso.id}>
+                        <td>{curso.codCurso}</td>
+                        <td>{curso.nomeCurso}</td>
+                        <td>{curso.período}</td>
+                         </tr>
+          )}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  render() { 
+            return (
+                <Main title={title}> 
+                {this.renderTable()} 
+                </Main> )
+}
+}
