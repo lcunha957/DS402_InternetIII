d7ed80faaadf8e8ea45aa690f557dbe71daec966
Lunara <88806865+lcunha957@users.noreply.github.com>
2022-09-22 01:47:32 -0300

    pag 24 apost4 react
    .

--- escola/src/App.js
+++ escola/src/App.js
@@ -2,19 +2,23 @@ import "./App.css";
 
 import Logo from "./components/template/Logo";
 import Menu from "./components/template/Menu";
-import Main from "./components/template/Main";
+//import Main from "./components/template/Main";
 import Footer from "./components/template/Footer";
-import CrudAluno from "./components/CrudAluno/CrudAluno";
+//import CrudAluno from "./components/CrudAluno/CrudAluno";
+import Rotas from './Rotas';
 
-function App() {
+import { BrowserRouter } from 'react-router-dom';
+
+ export default function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <Logo />
       <Menu />
-      <CrudAluno />
+      <Rotas />
       <Footer />
     </div>
+    </BrowserRouter>
   );
 }
 
-export default App;
