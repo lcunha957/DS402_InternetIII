568e6df66db4de1bc3f06b44651e58e19c30f055
Lunara <88806865+lcunha957@users.noreply.github.com>
2022-09-22 01:55:00 -0300

    rotas
    .

--- escola/src/Rotas.js
+++ escola/src/Rotas.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React from 'react';
 import { Routes, Route } from "react-router-dom";
-import Main from "./components/template/Main";
-import CrudAluno from "./components/CrudAluno/CrudAluno";
+
+import Main from './components/template/Main';
+import CrudAluno from './components/CrudAluno/CrudAluno';
 export default function Rotas() {
   return (
     <Routes>
       <Route
         exact
-        path="/"
+        path='/'
         element={
           <Main title="Bem Vindo!">
             <div>Cadastro de alunos, cursos e carômetro</div>
@@ -15,9 +16,9 @@ export default function Rotas() {
         }
       />
 
-      <Route path="/alunos" element={<CrudAluno />} />
+      <Route path='/alunos' element={<CrudAluno />} />
       <Route
-        path="*"
+        path='*'
         element={
           <Main title="Bem Vindo!">
             <div> Página não encontrada</div>
