038365999146812be7c997757691f79bf26f2673
Lunara <88806865+lcunha957@users.noreply.github.com>
2022-09-22 04:44:47 -0300

    homecontroller
    pag 12 apoatila1 aspnet

---
+++ ProjetoEscola_API/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoEscola_API.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public String Inicio()
+        {
+            return "Funcionou!";
+        }
+    }
+}
