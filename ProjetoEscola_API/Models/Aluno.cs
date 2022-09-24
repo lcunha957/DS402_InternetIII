8212d96c30922000132e920a207d04a3b443e607
Lunara Cunha <lunara.cunha@hotmail.com>
2022-09-24 04:15:27 -0300

    validação pt 2
    deixando a validação diretamente no formulário.

--- ProjetoEscola_API/Models/Aluno.cs
+++ ProjetoEscola_API/Models/Aluno.cs
@@ -7,16 +7,8 @@ namespace ProjetoEscola_API.Models{
     public class Aluno{
         
         public int id { get; set; }
-        [Required(ErrorMessage = "Ra com até 5 números")]
-        [StringLength(5)]
         public string? ra { get; set; }
-        [Required(ErrorMessage = "Nome com até 30 letras")]
-        [DisplayName("Nome do curso")]
-        [StringLength(30)]
         public string? nome { get; set; }
-        [Required(ErrorMessage = "Use as setas para ter o curso com 2 números")]
-        [Range(1,2)]
-        [DataType(DataType.Currency)]
         public int codCurso { get; set; }
         
         }
