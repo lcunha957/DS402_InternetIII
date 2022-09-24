1e8a270a11d78ebc97cc484fa174fff5f412f7a6
Lunara Cunha <lunara.cunha@hotmail.com>
2022-09-24 00:34:44 -0300

    pagina 4 - apostila 03 do dot.net
    Implementação dos métodos post, put, delete no AlunoController.cs

--- ProjetoEscola_API/Controllers/AlunoController.cs
+++ ProjetoEscola_API/Controllers/AlunoController.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProjetoEscola_API.Data;
 using ProjetoEscola_API.Models;
@@ -6,21 +10,58 @@ namespace ProjetoEscola_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AlunoController : ControllerBase 
+    public class AlunoController : Controller
     {
-        private EscolaContext _context; 
+        private readonly EscolaContext _context;
 
-        public AlunoController(EscolaContext context) 
-        {// construtor
-        _context = context;
+        public AlunoController(EscolaContext context)
+        { // construtor
+            _context = context;
         }
 
-        
-        
         [HttpGet]
-        public ActionResult<List<Aluno>> GetAll() 
+        public ActionResult<List<Aluno>> GetAll()
         {
-            return  _context.Aluno.ToList();
+            return _context.Aluno.ToList();
+        }
+
+        [HttpGet("{AlunoId}")]
+        public ActionResult<List<Aluno>> Get(int AlunoId)
+        {
+            try
+            {
+                var result = _context.Aluno.Find(AlunoId);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                return Ok(result);
+            }
+            catch
+            {
+                return this.StatusCode(
+                    StatusCodes.Status500InternalServerError,
+                    "Falha no acesso ao banco de dados."
+                );
             }
         }
-   }
+
+        [HttpPost]
+        public ActionResult post(Aluno model)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{AlunoRA}")]
+        public ActionResult put(string AlunoRA)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{AlunoRA}")]
+        public ActionResult delete(string AlunoRA)
+        {
+            return Ok();
+        }
+    }
+}
