c6ee5db10862c5f05422a928ccbf743b3da9ac02
Lunara Cunha <lunara.cunha@hotmail.com>
2022-09-24 01:09:01 -0300

    adicionando validações no model
    .

--- ProjetoEscola_API/Models/Aluno.cs
+++ ProjetoEscola_API/Models/Aluno.cs
@@ -1,11 +1,23 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
 namespace ProjetoEscola_API.Models{
     
     public class Aluno{
         
         public int id { get; set; }
-        
+        [Required(ErrorMessage = "Ra com até 5 números")]
+        [DisplayName("ra")]
+        [StringLength(5)]
         public string? ra { get; set; }
-        public string? nome { get; set; } 
+        [Required(ErrorMessage = "Nome com até 30 letras")]
+        [DisplayName("nome")]
+        [StringLength(30)]
+        public string? nome { get; set; }
+        [Required(ErrorMessage = "Use as setas para ter o curso com 2 números")]
+        [DisplayName("código do curso")]
+        [StringLength(2)] 
         public int codCurso { get; set; }
         
         }
