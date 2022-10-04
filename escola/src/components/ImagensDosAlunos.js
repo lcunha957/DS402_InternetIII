cf6c3d894c0ee9a8dfd3f5566b41a36163e99661
Lunara Cunha <lunara.cunha@hotmail.com>
2022-10-04 15:22:31 -0300

    arrumando array de imagens
    .

--- escola/src/components/ImagensDosAlunos.js
+++ escola/src/components/ImagensDosAlunos.js
@@ -1,33 +1,40 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
 import axios from 'axios';
 
 import Main from "./template/Main";
 
-const FonteAPIdeImagens = "https://pixabay.com/photos/search/";
+import '../components/ImagensDosAlunos.css';
 
-const ListaDeImagens = {
-    imagem: { id: "", url: "", width: 250, height: 100 },
-    listadefiguras: []
+const imagens=[
+    {id:"1", url:"https://cdn.pixabay.com/photo/2017/05/31/14/31/mango-2360551_960_720.jpg"},
+    {id:"2", url:"https://cdn.pixabay.com/photo/2016/07/22/09/59/fruit-1534493_960_720.jpg"},
+    {id:"3", url:"https://cdn.pixabay.com/photo/2019/04/28/18/34/orange-4163953_960_720.jpg"},
+    {id:"4", url:"https://cdn.pixabay.com/photo/2015/11/25/09/52/star-anise-1061572_960_720.jpg"},
+    {id:"5", url:"https://cdn.pixabay.com/photo/2016/10/27/22/52/apples-1776744_960_720.jpg"},
+    {id:"6", url:"https://cdn.pixabay.com/photo/2016/07/22/09/59/fruits-1534494_960_720.jpg"},
+    {id:"7", url:"https://cdn.pixabay.com/photo/2015/07/10/18/36/still-life-840014_960_720.jpg"},
+    {id:"8", url:"https://cdn.pixabay.com/photo/2021/01/05/05/30/grapes-5889697_960_720.jpg"}
+];
+
+const [imagem, setImagem] = useState();
+
+const handleChangeImagem=(e) =>{
+    setImagem(e.targetValue);
 }
 
+
+
 export default class ImagePerfil extends Component {
-    state = { ...ListaDeImagens }
+    state = { ...imagens }
 
-    componentDidMount() {
-        axios(FonteAPIdeImagens).then(resp => {
-            this.setState({ listadefiguras: resp.data })
-            
-        })
-    }
-
-    render() {
+  
+        render() {
         return (
             <div>
-            {this.state.listadefiguras.map(
-                (d) =>
-                <imagem id={ListaDeImagens.imagem.id}
-             src={d.url}/>
+            {this.state.imagens.map(
+                (f, i) =>
+                <img  id = {f.id} src={f.url} className="arara"/>
             )}
           </div>
         )
