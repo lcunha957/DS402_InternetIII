3788f4f2bd9dd1fd2758857b1f57d2d6a98bc8ab
Lunara Cunha <lunara.cunha@hotmail.com>
2022-10-03 21:09:42 -0300

    parte2 de atualizar....
    .

--- escola/src/Rotas.js
+++ escola/src/Rotas.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { Routes, Route } from "react-router-dom";
 
 import Main from './components/template/Main';
-<<<<<<< HEAD
+
+import CrudCadastroAluno from './components/CrudCadastroAluno';
 
 import CrudCurso from './components/CrudCurso';
-import CrudCadastroAluno from './components/CrudCadastroAluno';
-=======
-import CrudCurso from './components/CrudCurso';
-import CrudCadastroAluno from './components/CrudCadastroAluno'
->>>>>>> 3e1e2997cd92438e35ef2a43995271fab074f7f5
+
+
 
 export default function Rotas() {
   return (
@@ -24,13 +22,9 @@ export default function Rotas() {
         }
       />
 
-<<<<<<< HEAD
-      <Route path='/alunos' element={<CrudCadastroAluno />} />
-=======
 
       <Route path='/alunos' element={<CrudCadastroAluno/>} />
->>>>>>> 3e1e2997cd92438e35ef2a43995271fab074f7f5
-      
+
       <Route path='/cursos' element={<CrudCurso />} />
 
       <Route
