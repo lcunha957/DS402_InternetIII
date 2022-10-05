ab042f76b20c73fde7b05ab0ae6f33a104dac2dd
Lunara Cunha <lunara.cunha@hotmail.com>
2022-10-04 21:21:00 -0300

    initialState
    .

--- escola/src/components/Carometro.js
+++ escola/src/components/Carometro.js
@@ -37,7 +37,7 @@ const initialState= {
           
            
 export default class Carometro extends Component{
-    state={ ...initialState, ...Curso }
+    state={ ...initialState, ...Curso, ...figuras }
 
     
 componentDidMount() {
